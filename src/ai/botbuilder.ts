ea7b56b5fa68fb20402ef4c08d47bc72b3d41ec6
Utku TURKOGLU <uturkoglu@cinerpark.local>
2017-03-27 13:15:44 +0300

    added chat page

--- src/ai/botbuilder.ts
+++ src/ai/botbuilder.ts
@@ -12,7 +12,15 @@ export class BotBase {
                 appId: process.env.microsoft_appId,
                 appPassword: process.env.microsoft_appPass
             });
-
+            server.get('/api/messages', (req: restify.Request, res: restify.Response, next: restify.Next) => {
+                var body = "<html><head></head><body><iframe style='height:480px; width:402px' src='https://webchat.botframework.com/embed/aibrite?s=nDw65L-e3vM.cwA.BAs.72kXA9-9by8Z8vhkYr1SFWv54km16mi4lMUh1PEzAr0'></iframe></body></html>";
+                res.writeHead(200, {
+                    'Content-Length': Buffer.byteLength(body),
+                    'Content-Type': 'text/html'
+                });
+                res.write(body);
+                res.end();
+            })
             this.bot = new botbuilder.UniversalBot(connector);
             server.post('/api/messages', connector.listen());
             resolve();
